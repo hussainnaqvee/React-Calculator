6025c7b96b1f1043302efad48496f21b9036716e
Hussain <hussain.naqvee@gmail.com>
2022-09-26 23:09:36 +0500

    Updates

---
+++ precache-manifest.40b6fab3f1af8e60e12084c9b689f76c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "29758f799d268ede9323",
+    "url": "/React-Calculator/static/css/main.1aeb338d.chunk.css"
+  },
+  {
+    "revision": "29758f799d268ede9323",
+    "url": "/React-Calculator/static/js/main.6714ca15.chunk.js"
+  },
+  {
+    "revision": "e59843bcbdd3e73aba37",
+    "url": "/React-Calculator/static/js/runtime~main.f668fad0.js"
+  },
+  {
+    "revision": "91568b34f8e8624958a2",
+    "url": "/React-Calculator/static/js/2.ae733d23.chunk.js"
+  },
+  {
+    "revision": "c0c98a6c240aa4e0d888",
+    "url": "/React-Calculator/static/js/3.68b36b5f.chunk.js"
+  },
+  {
+    "revision": "f86e760a63dd54b5474c9a492abd47d3",
+    "url": "/React-Calculator/index.html"
+  }
+];
